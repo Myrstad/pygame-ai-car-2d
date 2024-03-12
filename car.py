4ba24e53241c6a8614ff2c2eda0cecc6e8e563c3
Myrstad <mortenmyrstad@gmail.com>
2024-03-12 15:27:24 +0100

    tried adding chevron to car

--- car.py
+++ car.py
@@ -15,6 +15,8 @@ class Car(object):
   def __init__(self, environment: Environment = Environment(), x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2) -> None:
     self.car_dim  = pg.Vector2(CAR_SIZE)
     self.direction = pg.Vector2(0, 1)
+    self.start_position = (x, y)
+    self.start_direction = (0, 1)
     self.center_position = pg.Vector2(x, y)
     self.velocity = pg.Vector2(0, 0)
     self.friction = CAR_FRICTION
@@ -25,27 +27,49 @@ class Car(object):
     self.sensor_directions = [0, -20, 20, 45, -45, 90,-90, 135, -135, 180]
     self.sensor_distance = 200
 
-  def draw(self, surface):
-    #debug draw
-    rotation = self.direction.angle_to(pg.Vector2(1,0)) # degrees
+  def reset(self) -> None:
+    self.crashed = False
+    self.center_position.x = self.start_position[0]
+    self.center_position.y = self.start_position[1]
+    self.direction.x = self.start_direction[0]
+    self.direction.y = self.start_direction[1]
+    self.velocity *= 0
 
+
+  def draw(self, surface, debug:bool = False):
     #draw "debug" lines
-    sensor_vectors = [self.direction.rotate(x)*self.sensor_distance for x in self.sensor_directions]
-    for vector in sensor_vectors:
-      line = Line(self.center_position, self.center_position + vector)
-      pg.draw.line(surface, BLACK, line.p1, line.p2)
-      intercepts = []
-      for env_line in self.environment.lines:
-        if line.intercepts(env_line):
-          intercepts.append(line.intercepts(env_line))
-          pg.draw.circle(surface, BLACK, line.intercepts(env_line)[1], 5)
-
-    
+    if debug:
+      sensor_vectors = [self.direction.rotate(x)*self.sensor_distance for x in self.sensor_directions]
+      for vector in sensor_vectors:
+        line = Line(self.center_position, self.center_position + vector)
+        pg.draw.line(surface, BLACK, line.p1, line.p2)
+        intercepts = []
+        for env_line in self.environment.lines:
+          if line.intercepts(env_line):
+            intercepts.append(line.intercepts(env_line))
+            # pg.draw.circle(surface, BLACK, line.intercepts(env_line)[1], 5)
+        
+        if len(intercepts) > 0:
+          intercepts.sort(key=lambda x: x[0])
+          pg.draw.circle(surface, BLACK, intercepts[0][1], 5)
     
     #draw car
     points = self.get_points()
     clr = RED if not self.crashed else BLUE
     pg.draw.polygon(surface, clr, points)
+
+    #draw chevron in direction
+    points = []
+    w = self.car_dim[0] #width of car
+    h = self.car_dim[1] #width of car
+    cpx = self.center_position[0] #center position x
+    cpy = self.center_position[1] #center position y
+    rotation = self.direction.angle_to(pg.Vector2(1,0)) # degrees
+    points = [(cpx-w//3, cpy), (cpx, cpy+cpy//4), (cpx+w//3, cpy)]
+    points = [rotate_point(p, self.center_position, -rotation) for p in points]
+    pg.draw.lines(surface, BLACK, False, points)
+
+
 
   def get_points(self) -> list[tuple[int]]:
     rotation = self.direction.angle_to(pg.Vector2(1,0)) # degrees
@@ -71,24 +95,25 @@ class Car(object):
 
 
   def update(self, keys:dict):
-    if keys[pg.K_w] or keys[pg.K_UP]:
+    if (keys[pg.K_w] or keys[pg.K_UP]) and not self.crashed:
       self.velocity += self.direction * self.acceleration
-    if keys[pg.K_s] or keys[pg.K_DOWN]:
+    if (keys[pg.K_s] or keys[pg.K_DOWN]) and not self.crashed:
       self.velocity -= self.direction * self.acceleration
-    if keys[pg.K_a] or keys[pg.K_LEFT]:
+    if (keys[pg.K_a] or keys[pg.K_LEFT]) and not self.crashed:
       self.direction = self.direction.rotate(-self.turning_speed)
-    if keys[pg.K_d] or keys[pg.K_RIGHT]:
+    if (keys[pg.K_d] or keys[pg.K_RIGHT]) and not self.crashed:
       self.direction = self.direction.rotate(self.turning_speed)
+    if keys[pg.K_r]:
+      self.reset()
     
     self.velocity *= self.friction
-
+  
     self.center_position += self.velocity
 
     # get points
     points = self.get_points()
     lines = Car.make_lines(points)
 
-    self.crashed = False
     for car_line in lines:
       for env_line in self.environment.lines:
         if car_line.intercepts(env_line) != False:
